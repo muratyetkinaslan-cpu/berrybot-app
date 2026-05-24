7c06c0b3baa5c050201bed1f0662b4f057849f12
muratyetkinaslan-cpu <muratyetkinaslan@gmail.com>
2026-05-24 11:22:56 +0300

    a

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js';
-const SUPABASE_URL = "https://byjxolgvqetwoxhcaemv.spabase.co";
+const SUPABASE_URL = "https://byjxolgvqetwoxhcaemv.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_hCkrcWh7auiz-tdqEfUp5Q_xgOWOMYz"
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
